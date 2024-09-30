b2c67e4e593806b6cd28798071db33aa4748ab3d
Nickey302 <caproson6@gmail.com>
2024-09-30 19:52:41 +0900

    Feat: Main-page 초기 GPGPU구현완료

--- src/components/main/Experience.jsx
+++ src/components/main/Experience.jsx
@@ -1,23 +1,47 @@
-'use client'
-
-import { Float, Text, OrbitControls } from "@react-three/drei";
+import { OrbitControls, CameraShake } from '@react-three/drei';
+import { useControls } from 'leva';
+import { Particles } from './Particles.jsx';
+import { Float, Text } from '@react-three/drei';
+import { Perf } from 'r3f-perf'
 import Ocean from './Water.jsx';
-//
-//
-//
+
 export default function Experience() {
+  const props = useControls({
+    focus: { value: 5.1, min: 3, max: 7, step: 0.01 },
+    speed: { value: 100, min: 0.1, max: 100, step: 0.1 },
+    aperture: { value: 1.8, min: 1, max: 5.6, step: 0.1 },
+    fov: { value: 20, min: 0, max: 200 },
+    curl: { value: 0.25, min: 0.01, max: 0.5, step: 0.01 },
+  });
+
   return (
     <>
-      <color args={['#626A88']} attach="background" />
-      <OrbitControls makeDefault />
-      <ambientLight intensity={ 1.5 } />
+      {/* <Perf position="top-left"/> */}
+
+      <color args={['black']} attach="background" />
+      <OrbitControls
+        makeDefault
+        // autoRotate
+        // autoRotateSpeed={0.5}
+        zoomSpeed={0.1}
+      />
+      <CameraShake
+        yawFrequency={1}
+        maxYaw={0.05}
+        pitchFrequency={1}
+        maxPitch={0.05}
+        rollFrequency={0.5}
+        maxRoll={0.5}
+        intensity={0.2}
+      />
+
+      <ambientLight intensity={1.5} />
       <directionalLight />
-      
-      <Float>
+
         <Text
-          position={[0, 20, 0]}
-          font='./fonts/Montserrat-VariableFont_wght.ttf'
-          fontSize={20}
+          position={[0, 0, 2]}
+          font="./fonts/Montserrat-VariableFont_wght.ttf"
+          fontSize={0.3}
           color="white"
           anchorX="center"
           anchorY="middle"
@@ -26,9 +50,11 @@ export default function Experience() {
         >
           CORDIVE
         </Text>
-      </Float>
 
-      <Ocean />
+      {/* <Ocean /> */}
+
+      {/* 파티클 추가 */}
+      <Particles {...props} />
     </>
   );
 }
---
+++ src/utils/main/getSphere.jsx
@@ -0,0 +1,13 @@
+import * as THREE from 'three';
+
+function getPoint(v, size, data, offset) {
+  v.set(Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1);
+  if (v.length() > 1) return getPoint(v, size, data, offset);
+  return v.normalize().multiplyScalar(size).toArray(data, offset);
+}
+
+export function getSphere(count, size, p = new THREE.Vector4()) {
+  const data = new Float32Array(count * 4);
+  for (let i = 0; i < count * 4; i += 4) getPoint(p, size, data, i);
+  return data;
+}

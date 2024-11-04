e67a8d39bdb905c71c0b351848e0ba2a4fc6a2e9
Nickey302 <caproson6@gmail.com>
2024-11-05 01:54:50 +0900

    Chore: package.json 수정

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -8,3 +8,37 @@ if (!supabaseUrl || !supabaseKey) {
 }
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+/**
+ * DB SCHEMA
+ */
+// CREATE TABLE `objects` (
+//   `keyword` varchar(255),
+//   `xpos` float,
+//   `ypos` float,
+//   `zpos` float,
+//   `user_id` integer,
+//   `created_at` timestamp
+// );
+
+// CREATE TABLE `users` (
+//   `id` integer PRIMARY KEY,
+//   `username` varchar(255),
+//   `created_at` timestamp
+// );
+
+// CREATE TABLE `responses` (
+//   `id` integer PRIMARY KEY,
+//   `q1` integer,
+//   `q2` integer,
+//   `body1` text,
+//   `body2` text,
+//   `body3` text,
+//   `user_id` integer,
+//   `created_at` timestamp
+// );
+
+// ALTER TABLE `responses` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+// ALTER TABLE `objects` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
